8b446ffe28898826df672bb07dceb0e85748f8f1
Matthew Kudija <m.kudija@gmail.com>
2016-12-31 11:29:56 -0500

    update range ring script

--- plot_range_rings.py
+++ plot_range_rings.py
@@ -33,7 +33,7 @@ m.drawstates(linewidth=0.25, color='#A8A8A8', zorder=6)
 m.drawcountries(linewidth=0.25, color='#707070', zorder=7)
 
 #drawmapscale(lon, lat, lon0, lat0, length, barstyle=’simple’, units=’km’, fontsize=9, yoffset=None, labelstyle=’simple’, fontcolor=’k’, fillcolor1=’w’, fillcolor2=’k’, ax=None, format=’%d’, zorder=None)
-m.drawmapscale(lon=urcrnrlon-2, lat=llcrnrlat+.6, lon0=lon_0, lat0=lat_0, length=200, barstyle='fancy', 
+m.drawmapscale(lon=urcrnrlon-3.25, lat=llcrnrlat+.6, lon0=lon_0, lat0=lat_0, length=200, barstyle='fancy', 
                 units='nmi', fontsize=6, fillcolor1='w', fillcolor2='#484848')
 
 for row in range(0,df.shape[0]): 
@@ -51,7 +51,7 @@ for row in range(0,df.shape[0]):
 
     # Marker Parameters
     zoom         = 1
-    zoom_orig    = .4
+    zoom_orig    = .5
     zoom_des     = .25
     lbl_offset_x = 20000
     lbl_offset_y = -5000
@@ -60,26 +60,30 @@ for row in range(0,df.shape[0]):
     x, y = m(lng_orig,lat_orig)
     #m.scatter(x,y,2,marker='.',edgecolors='#CF5300',c='#CF5300', zorder=10)
     plt.text(x+lbl_offset_x, y+lbl_offset_y, name_orig, fontsize=5, weight='bold')#, backgroundcolor='#FFFFFF')#, ha='center')
-    logo = read_png('data/helipad1.png')
+    logo = read_png('data/airport1.png')
     imagebox = OffsetImage(logo, zoom=zoom*zoom_orig)        
     xy = [x, y]
     ab = AnnotationBbox(imagebox, xy, xybox=(0, 0), xycoords='data', boxcoords='offset points', frameon=False)
     ax.add_artist(ab)
 
+
+    # Hide mark at destination for now
+    '''
     # Marker at Destination
     x, y = m(lng_des,lat_des)
     plt.text(x+lbl_offset_x, y+lbl_offset_y, name_des, fontsize=5)#, ha='center')
     #m.scatter(x,y,2,marker='.',edgecolors='#CF5300',c='#CF5300', zorder=10)
-    logo = read_png('data/rig2.png')
+    logo = read_png('data/rig1.png')
     imagebox = OffsetImage(logo, zoom=zoom*zoom_des)        
     xy = [x, y]
     ab = AnnotationBbox(imagebox, xy, xybox=(0, 0), xycoords='data', boxcoords='offset points', frameon=False)
     ax.add_artist(ab)
+    '''
 
     # Range rings around origin
     # tissot(lon_0, lat_0, radius_deg, npts, ax=None, **kwargs)
     # use patches for properties: http://matplotlib.org/api/patches_api.html
-    ranges = [200] # in nautical miles
+    ranges = [200] # in nautical miles, add other distances to list as desired
     for range in ranges:
         deg = range/69*1.15078 # converts nautical miles to degrees
         m.tissot(lng_orig, lat_orig, radius_deg=deg, npts=50, edgecolor='#2E5FAC', fill=False, alpha=1, linestyle=':')
